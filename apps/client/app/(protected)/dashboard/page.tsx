c5c96bac5e8de0533c3a0da917f4112fef7b61a9
Rahul Yadav <ry.rahulmyadav@gmail.com>
2025-09-22 21:38:28 +0530

    feat: initialised konva.js

---
+++ apps/client/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,5 @@
+import { DashboardPage as DashboardPageComponent } from '@/modules/dashboard/dashboard-page';
+
+export default function DashboardPage() {
+  return <DashboardPageComponent />;
+}
---
+++ apps/client/modules/dashboard/dashboard-page.tsx
@@ -0,0 +1,10 @@
+import { Layout } from './components/layout';
+import { StageComponent } from './components/stage';
+
+export const DashboardPage = () => {
+  return (
+    <Layout>
+      <StageComponent />
+    </Layout>
+  );
+};
